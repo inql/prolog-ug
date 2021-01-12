03240bc61ec4970922503b7902670bf6f555ebce
inql <dbinkus96@gmail.com>
2021-01-12 21:56:48 +0100

    Add zad8.pl

---
+++ zad8.pl
@@ -0,0 +1,6 @@
+append2([X|L1],L2,[X|L3]) :- write(X), write(L1), write(L2), write(L3),nl, append2(L1,L2,L3).
+append2([],L,L).   
+
+% ?- append2(X,[3,4],[2,3,4]).
+% X = [2]
+% nie mam pojecia dlaczego
